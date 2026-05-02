b4888f5b37b510d91966d6e5b5eb7b888ee45701
vicians <samuelvcallegaro@gmail.com>
2026-05-02 15:03:23 -0300

    feat: separa rotas de webhooks e implementa envio de mensagens whatsapp

--- backend/src/webhook/index.ts
+++ backend/src/webhook/index.ts
@@ -1,29 +1,12 @@
 import { Router } from 'express';
+import { validateWebhook, handleMessage } from './controller';
 
 const router = Router();
 
-// Esta é a rota que a Meta acessa para validar o seu webhook
-router.get('/', (req, res) => {
-    const VERIFY_TOKEN = process.env.VERIFY_TOKEN; // Ele busca do seu .env
+// Rota GET: Validação do Webhook (Handshake com a Meta)[cite: 1]
+router.get('/', validateWebhook);
 
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    if (mode && token) {
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            console.log('✅ WEBHOOK_VERIFIED');
-            return res.status(200).send(challenge); // Retorna apenas o challenge puro
-        } else {
-            return res.sendStatus(403); // Token errado
-        }
-    }
-});
-
-// Aqui você deve ter a sua rota POST para receber as mensagens reais depois
-router.post('/', (req, res) => {
-    // Lógica para processar as mensagens da CicloOficina
-    res.sendStatus(200);
-});
+// Rota POST: Recebimento e processamento das mensagens[cite: 1]
+router.post('/', handleMessage);
 
 export default router;
