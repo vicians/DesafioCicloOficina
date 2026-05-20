2c36b8642bd91a94da90b15e2438770168101675
davims9 <davimourasouza@gmail.com>
2026-05-20 15:39:38 -0300

    fix: ajuste acesso barras

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,6 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Middleware para normalizar barras duplas na URL geradas pelo Proxy Reverso do Gateway
+app.use((req, res, next) => {
+  if (req.url.startsWith('//')) {
+    req.url = req.url.replace(/^\/+/, '/');
+  }
+  next();
+});
+
 if (process.env.NODE_ENV !== 'production') {
   app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
